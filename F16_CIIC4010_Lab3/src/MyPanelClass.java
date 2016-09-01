aafba21fd54c5f6d5861da88f7490a57aa54f5cb
Ivan <ivan.santiago6@upr.edu>
2016-09-01 13:12:04 -0400

    Changes 6.0: oval centered and finished!

--- F16_CIIC4010_Lab3/src/MyPanelClass.java
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -20,19 +20,18 @@ public class MyPanelClass extends JPanel {
                         int y2 = getHeight() - myInsets.bottom - 1;
                         int width = x2 - x1;
                         int height = y2 - y1;
-                        //int x_middle = (x2-x1)/2, y_middle=(y2-y1)/2;
                         
                         //Paint the background
                         g.setColor(Color.BLACK);
                         g.fillRect(x1, y1, width + 1, height + 1);
                         
-//                      //Draw a border
-//                        g.setColor(Color.YELLOW);
-//                        g.drawRect(x1, y1, width, height);
-//                        g.setColor(Color.RED);
-//                        g.drawRect(x1+5, y1+5, width-10, height-10);
-//                        
-//                       //Draw lines
+                        //Draw a border
+                        g.setColor(Color.YELLOW);
+                        g.drawRect(x1, y1, width, height);
+                        g.setColor(Color.RED);
+                        g.drawRect(x1+5, y1+5, width-10, height-10);
+                       
+                        //Draw lines
                         g.setColor(Color.WHITE);
                         g.drawLine(x1, y1, x2, y2);
                         g.setColor(Color.BLUE);
@@ -40,8 +39,7 @@ public class MyPanelClass extends JPanel {
                         
                         //Oval
                         g.setColor(Color.LIGHT_GRAY);
-                        g.fillOval(width/2, height/2, 55, 55);
-                        
+                        g.fillOval(width/2 - 55/2, height/2 - 55/2, 55, 55);
                         
                         
                       
